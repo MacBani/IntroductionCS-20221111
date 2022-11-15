99470f743814e9a5e007ae8de1a7b7f936e58ee2
Mac_bani <nikita@mail.ru>
2022-11-15 18:25:54 +0300

    HomeWork№1

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a*a;
+System.Console.WriteLine($"{a}^2={b}");
