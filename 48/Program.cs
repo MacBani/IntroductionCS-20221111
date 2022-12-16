9d5f85ef10d1ca6d53944eb7c3dfe0d4981a6f4f
Mac_bani <you@example.com>
2022-12-16 17:25:28 +0300

    ДЗ №5

---
+++ 48/Program.cs
@@ -0,0 +1,34 @@
+﻿// 48.	Написать программу копирования массива
+
+int N = 5;
+int[] m = new int[N];
+putM(m);
+int[] copym = copyM(m);
+showM(m);
+showM(copym);
+
+
+int[] copyM(int[] ma)
+{
+    int[] mas = new int[ma.Length];
+    ma.CopyTo(mas, 0);
+    return mas;
+}
+
+void putM(int[] m)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < m.Length; i++)
+    {
+        m[i] = rnd.Next(0, 9);
+    }
+}
+
+void showM(int[] m)
+{
+    for (int i = 0; i < m.Length; i++)
+    {
+        Console.Write(m[i] + " ");
+    }
+    Console.WriteLine();
+}
