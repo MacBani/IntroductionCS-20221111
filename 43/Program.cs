7308e830b4acbb91f5ffa564a23b52d9f1d40e8b
Mac_bani <you@example.com>
2022-12-16 17:26:14 +0300

    ДЗ №5

---
+++ 43/Program.cs
@@ -0,0 +1,27 @@
+﻿// 43.	С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] ma = new int[N];
+int count = 0;
+System.Console.WriteLine($"Введите {N} чисел: ");
+for (int i = 0; i < N; i++)
+{
+    ma[i] = Convert.ToInt32(Console.ReadLine());
+    if (ma[i] > 0)
+    {
+        count++;
+    }
+}
+
+showM(ma);
+System.Console.WriteLine($"Количество введёных положительных чисел: {count} ");
+
+
+void showM(int[] m)
+{
+    for (int i = 0; i < m.Length; i++)
+    {
+        System.Console.Write(m[i] + " ");
+    }
+    System.Console.WriteLine();
+}
---
+++ 46/Program.cs
@@ -0,0 +1,39 @@
+﻿// 46.	С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+int N = 0;
+while (N < 2)
+{
+    Console.WriteLine("Введите число больше единицы: ");
+    N = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] ma = new int[N];
+
+funcF(ma);
+showM(ma);
+
+
+
+void funcF(int[] m)
+{
+    int size = m.Length;
+    m[0] = 0;
+    m[1] = 1;
+
+    if (size > 2)
+    {
+        for (int i = 2; i < size; i++)
+        {
+            m[i] = m[i - 1] + m[i - 2];
+        }
+    }
+
+}
+
+void showM(int[] m)
+{
+    for (int i = 0; i < m.Length; i++)
+    {
+        Console.Write(m[i] + " ");
+    }
+    Console.WriteLine();
+}
